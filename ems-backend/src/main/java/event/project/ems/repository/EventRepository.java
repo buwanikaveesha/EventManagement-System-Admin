c8395050388cb931c22b854c0026398c7391fa61
Buwani Hemachandra <135821217+buwanikaveesha@users.noreply.github.com>
2024-11-08 13:13:47 +0530

    Delete ems-backend directory

--- ems-backend/src/main/java/event/project/ems/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package event.project.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import event.project.ems.entity.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long>{
-    
-}
